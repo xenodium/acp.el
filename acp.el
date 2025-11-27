8f82c9a65bb44ba2aad452bfa2a62c443bc12fb1
xenodium <8107219+xenodium@users.noreply.github.com>
2025-11-27 16:49:26 +0000

    Bumping acp.el version
    https://github.com/xenodium/agent-shell/issues/143

--- acp.el
+++ acp.el
@@ -4,10 +4,10 @@
 
 ;; Author: Alvaro Ramirez https://xenodium.com
 ;; URL: https://github.com/xenodium/acp.el
-;; Version: 0.7.2
+;; Version: 0.7.3
 ;; Package-Requires: ((emacs "28.1"))
 
-(defconst acp-package-version "0.7.2")
+(defconst acp-package-version "0.7.3")
 
 ;; This package is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
