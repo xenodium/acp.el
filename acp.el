771e01e8605c7ce1b9618ba503a8f4139cbb4f95
xenodium <8107219+xenodium@users.noreply.github.com>
2026-02-01 19:11:39 +0000

    Bumping version

--- acp.el
+++ acp.el
@@ -4,10 +4,10 @@
 
 ;; Author: Alvaro Ramirez https://xenodium.com
 ;; URL: https://github.com/xenodium/acp.el
-;; Version: 0.8.3
+;; Version: 0.9.1
 ;; Package-Requires: ((emacs "28.1"))
 
-(defconst acp-package-version "0.8.3")
+(defconst acp-package-version "0.9.1")
 
 ;; This package is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
